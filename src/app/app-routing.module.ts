8831d81b05988089d077f87a7a8a5f2e3ae01a15
Rafael Lima <rafaa99@gmail.com>
2022-07-04 16:23:07 -0300

    add login guard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {
@@ -11,12 +13,16 @@ const routes: Routes = [
     path: 'home',
     // lazy loading home module
     loadChildren: async () => (await import('./home/home.module')).HomeModule,
+    // login guard redirect to: 'animais ~> already logged in
+    canLoad: [LoginGuard],
   },
   {
     path: 'animais',
     // lazy loading animais module
     loadChildren: async () =>
       (await import('./animais/animais.module')).AnimaisModule,
+    // auth guard redirect to: 'home' ~> must be logged in
+    canLoad: [AutenticacaoGuard],
   },
 ];
 
