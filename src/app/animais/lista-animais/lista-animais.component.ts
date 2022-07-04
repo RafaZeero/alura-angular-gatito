b3a5219cf65fb8b82491529913a64c9c69059620
Rafael Lima <rafaa99@gmail.com>
2022-07-04 15:49:18 -0300

    using Angular to determine life cycle & using rxjs to fetch animal data from backend

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Animais } from '../animais';
 import { AnimaisService } from '../animais.service';
@@ -9,7 +10,7 @@ import { AnimaisService } from '../animais.service';
   styleUrls: ['./lista-animais.component.scss'],
 })
 export class ListaAnimaisComponent implements OnInit {
-  animais!: Animais;
+  animais$!: Observable<Animais>;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -17,14 +18,24 @@ export class ListaAnimaisComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // subscribe to user
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      // fetch username
-      const username = usuario.name ?? '';
-      // fetch username animal list
-      this.animaisService.listaDoUsuario(username).subscribe((animais) => {
-        this.animais = animais;
-      });
-    });
+    // // subscribe to user
+    // this.usuarioService.retornaUsuario().subscribe((usuario) => {
+    //   // fetch username
+    //   const username = usuario.name ?? '';
+    //   // fetch user (username) animal list
+    //   this.animaisService.listaDoUsuario(username).subscribe((animais) => {
+    //     this.animais = animais;
+    //   });
+    // });
+
+    // using Rxjs
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      // switch data flow -> from user to animal list
+      switchMap((usuario) => {
+        const username = usuario.name ?? '';
+        // fetch user (username) animal list
+        return this.animaisService.listaDoUsuario(username);
+      })
+    );
   }
 }
