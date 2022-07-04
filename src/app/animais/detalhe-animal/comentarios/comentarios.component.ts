6b78f4c278aabface584ca460e9fec3da2fb067d
Rafael Lima <rafaa99@gmail.com>
2022-07-04 17:44:38 -0300

    register comment in cat photo

--- src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, switchMap, tap } from 'rxjs';
 import { Comentarios } from './comentarios';
+import { ComentariosService } from './comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
@@ -8,9 +10,40 @@ import { Comentarios } from './comentarios';
   styleUrls: ['./comentarios.component.scss'],
 })
 export class ComentariosComponent implements OnInit {
+  @Input() ID!: number;
+
   comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
 
-  constructor() {}
+  constructor(
+    private comentarioService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // initiate comments
+    this.comentarios$ = this.comentarioService.buscaComentario(this.ID);
+
+    // create form
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
+  }
+
+  gravar(): void {
+    // get comment
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+
+    // refresh page for new comment
+    this.comentarios$ = this.comentarioService
+      .incluiComentario(this.ID, comentario)
+      .pipe(
+        switchMap(() => this.comentarioService.buscaComentario(this.ID)),
+        tap(() => {
+          // reset comment form
+          this.comentarioForm.reset();
+          alert('Comentário enviado!');
+        })
+      );
+  }
 }
