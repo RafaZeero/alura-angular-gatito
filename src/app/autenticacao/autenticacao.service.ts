a5abf347a3fb734323bd82c15ddee3b15ed8be17
Rafael Lima <rafaa99@gmail.com>
2022-07-04 11:49:08 -0300

    add auth service

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,19 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
   apiUrl = 'http://localhost:3000/user/login';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
   // authentication from form to connect to api
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.http.post(this.apiUrl, {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.http
+      .post(
+        this.apiUrl,
+        {
+          // send body to request
+          userName: usuario,
+          password: senha,
+        },
+        {
+          // get headers from request
+          observe: 'response',
+        }
+      )
+      .pipe(
+        tap((response) => {
+          // get token in header
+          const authToken = response.headers.get('x-access-token') ?? '';
+          // save token in fn 'salvaToken'
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
