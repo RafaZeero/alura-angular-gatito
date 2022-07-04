50e46a50e11304f7ba04f134cadfa0d7a53dccce
Rafael Lima <rafaa99@gmail.com>
2022-07-04 17:04:09 -0300

    add delete animal & like animal fn logic

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
 import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,22 @@ export class AnimaisService {
 
   buscaPorID(ID: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${ID}`);
+  }
+
+  excluiAnimal(ID: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${ID}`);
+  }
+
+  curtir(ID: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${ID}/likes`, {}, { observe: 'response' })
+      .pipe(
+        map(() => true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED
+            ? of(false)
+            : throwError(() => new Error(error.message));
+        })
+      );
   }
 }
