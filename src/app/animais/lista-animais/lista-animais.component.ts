31d06e6ba858c9026f2f5125bd23abd70a08007a
Rafael Lima <rafaa99@gmail.com>
2022-07-04 15:39:26 -0300

    render user animal list

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.scss']
+  styleUrls: ['./lista-animais.component.scss'],
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais!: Animais;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
   ngOnInit(): void {
+    // subscribe to user
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      // fetch username
+      const username = usuario.name ?? '';
+      // fetch username animal list
+      this.animaisService.listaDoUsuario(username).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
   }
-
 }
