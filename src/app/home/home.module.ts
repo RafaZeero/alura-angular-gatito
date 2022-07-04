2ba56eac5762c789cd3912b9e4dd0da2b64c5e74
Rafael Lima <rafaa99@gmail.com>
2022-07-03 22:10:33 -0300

    add form validity - required; add btn tgl disabled/enabled validity

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
