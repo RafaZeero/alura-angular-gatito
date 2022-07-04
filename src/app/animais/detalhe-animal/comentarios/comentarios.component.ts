a6b87cbc90da717277d6cea8917fbca4f326d72a
Rafael Lima <rafaa99@gmail.com>
2022-07-04 17:37:26 -0300

    add comment template form

--- src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comentarios } from './comentarios';
 
 @Component({
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
-  styleUrls: ['./comentarios.component.scss']
+  styleUrls: ['./comentarios.component.scss'],
 })
 export class ComentariosComponent implements OnInit {
+  comentarios$!: Observable<Comentarios>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
